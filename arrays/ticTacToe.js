5d167357b8842eda89439fb562f8b392ac53b737
j21winter <j21winter@gmail.com>
2024-01-11 17:27:58 -0600

    sudocode

---
+++ arrays/ticTacToe.js
@@ -0,0 +1,46 @@
+// For this weekend challenge, consider the game Tic-Tac-Toe.
+// How would you represent a 3x3 game board: specifically, whether each square is unoccupied or claimed by a player? 
+// Could you represent this with an array? 
+// What would the array contain? 
+// Is there a benefit from having an array of arrays?
+
+// Once you have a good design in mind, create a function that, given one of these gameboards,
+// automatically determines and returns the overall state of the game. 
+// A game is always in one of five different states – 
+    // one state might be “Player 1’s turn”; 
+    // another might be “Game over: player 2 has won.”
+
+// Once you have mastered that, can you extend this function to handle gameboards of arbitrary size?
+
+const gameBoard = [ 
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+                ]
+
+function ticTacToe(){
+    // State will change based on the status of the game
+    let state = ended 
+
+    let player1, player2
+
+    // let players pick X or O
+    // let the players pick who starts
+
+    while(state = active){
+        
+    }
+    // enter the game
+    // print the board with ABC x and 123 y to identify the squares
+    // promt() for input
+    // validate input
+    // run a check for a three in a row
+    // ask for next player turn.
+
+    // once a winner has been determined, or board has been filled GAME OVER
+    // offer restart
+    
+
+}
+
+ticTacToe()
